d2bbee27f32e90ba11696c2944b35f77584b3416
carlosmsx <carlosmsx@gmail.com>
2022-09-21 19:12:01 -0300

    implementada pagina de administracion

--- src/components/views/receta/AdministrarReceta.jsx
+++ src/components/views/receta/AdministrarReceta.jsx
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Swal from "sweetalert2";
+import { Table } from "react-bootstrap";
+import ItemReceta from "./ItemReceta";
+import { Link } from "react-router-dom";
 
 const AdministrarReceta = () => {
+    const API_URL = process.env.REACT_APP_API_URL;
+
+    const [recetas, setRecetas] = useState([]);
+
+    useEffect(() => {
+        consultarAPI();
+    }, []);
+
+    const consultarAPI = async () => {
+        try {
+            //GET
+            const respuesta = await fetch(API_URL);
+            const listaRecetas = await respuesta.json();
+            setRecetas(listaRecetas);
+        } catch (error) {
+            console.log(error);
+            //mostrar mensaje: espere unos minutos y vuelva a intentarlo
+        }
+    };
+
     return (
-        <div>
-            <h2>Adm Recetas</h2>
-        </div>
+        <section className="container">
+            <div className="d-flex justify-content-between align-item-center mt-5">
+                <h1 className="display-4">Administrador de recetas</h1>
+                <div className="d-flex flex-column justify-content-center">
+                    <Link to="/receta/crear" className="btn btn-primary">
+                        Agregar
+                    </Link>
+                </div>
+            </div>
+            <hr />
+            <Table responsive striped bordered hover>
+                <thead>
+                    <tr>
+                        {/* <th>Cod</th> */}
+                        <th>Receta</th>
+                        {/* <th>Imagen</th> */}
+                        <th>Ingredientes</th>
+                        <th>Instrucciones</th>
+                        <th>Opciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recetas.map((receta) => (
+                        <ItemReceta key={receta._id} receta={receta} consultarAPI={consultarAPI} />
+                    ))}
+                </tbody>
+            </Table>
+        </section>
     );
 };
 
--- src/components/views/receta/ItemReceta.jsx
+++ src/components/views/receta/ItemReceta.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import Swal from "sweetalert2";
+import {Link} from 'react-router-dom';
 
-const ItemReceta = () => {
+const ItemReceta = ({ receta, consultarAPI }) => {
+    const { nombre, _id, imagen, ingredientes, instrucciones } = { ...receta };
+    const URL_API = process.env.REACT_APP_API_CAFETERIA;
+
+    const handleDelete = () => {
+        Swal.fire({
+            title: "Está seguro?",
+            text: "no podrá deshacer esta operación!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Borrar",
+            cancelButtonText: "Cancelar",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    //realizar peticion DELETE
+                    const respuesta = await fetch(URL_API+'/'+_id,{
+                        method: "DELETE"
+                    });
+
+                    if (respuesta.status ===200)
+                    {
+                        Swal.fire("Receta eliminada!", "La receta fue correctamente eliminada.", "success");
+                    }
+                    //recargar tabla de recetas
+                    consultarAPI();
+                } catch (error) {
+                    Swal.fire("Error al eliminar", "Se produjo un error al intentar eliminar la receta, por favor aguarde unos minutos e intente nuevamente", "error");
+                }
+            }
+        });
+    };
     return (
-        <div>
-            
-        </div>
+        <tr>
+            {/* <td>{_id}</td> */}
+            <td>{nombre}</td>
+            {/* <td>{imagen}</td> */}
+            <td>{ingredientes}</td>
+            <td>{instrucciones}</td>
+            <td>
+                <Link className="btn btn-success" to={`/receta/detalle/${_id}`}>Detalle</Link>
+                <Link className="btn btn-warning" to={`/receta/editar/${_id}`}>Editar</Link>
+                <Button variant="danger" onClick={handleDelete}>
+                    Borrar
+                </Button>
+            </td>
+        </tr>
     );
 };
 
